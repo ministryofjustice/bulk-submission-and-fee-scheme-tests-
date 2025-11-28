5068bfe236f600832f73c9886f5831b28f4782b6
Jamie Briggs <jamie.briggs@justice.gov.uk>
2025-11-28 11:34:34 +0000

    Added stable cucumber module, and updated configuration

--- cucumber.js
+++ cucumber.js
@@ -1,26 +1,36 @@
 module.exports = {
-   bulksubmission: [
-      "--require-module ts-node/register",
-      "--require tests/features/**/*.ts",
-      "--format json:reports/cucumber.json",
-      "--format html:reports/cucumber.html",
-      "--tags @bulkSubmission",
-      "tests/features/**/BulkSubmission/*.feature"
-    ].join(" "),
-    temp: [
-      "--require-module ts-node/register",
-      "--require tests/features/**/*.ts",
-      "--format json:reports/cucumber.json",
-      "--format html:reports/cucumber.html",
-      "--tags @temp",
-      "tests/features/**/BulkSubmission/*.feature"
-    ].join(" "),
-    api: [
-      "--require-module ts-node/register",
-      "--require tests/features/**/*.ts",
-      "--format json:reports/cucumber.json",
-      "--format html:reports/cucumber.html",
-      "--tags @api",
-      "tests/features/**/*.feature"
-    ],
+  bulksubmission: [
+    "--require-module ts-node/register",
+    "--require tests/features/**/*.ts",
+    "--format json:reports/cucumber.json",
+    "--format html:reports/cucumber.html",
+    "--tags @bulkSubmission",
+    "tests/features/**/BulkSubmission/*.feature"
+  ].join(" "),
+  temp: [
+    "--require-module ts-node/register",
+    "--require tests/features/**/*.ts",
+    "--format json:reports/cucumber.json",
+    "--format html:reports/cucumber.html",
+    "--tags @temp",
+    "tests/features/**/BulkSubmission/*.feature"
+  ].join(" "),
+  stable: [
+    "--require-module ts-node/register",
+    "--require tests/features/**/*.ts",
+    "--format json:reports/cucumber.json",
+    "--format html:reports/cucumber.html",
+    "--tags @stable",
+    "--parallels 4",
+    "--retries 2",
+    "tests/features/**/BulkSubmission/*.feature"
+  ].join(" "),
+  api: [
+    "--require-module ts-node/register",
+    "--require tests/features/**/*.ts",
+    "--format json:reports/cucumber.json",
+    "--format html:reports/cucumber.html",
+    "--tags @api",
+    "tests/features/**/*.feature"
+  ],
 };
