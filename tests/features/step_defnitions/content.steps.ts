8ad65a5a3ed3cdd68e6447d8ecb4e1566e39d397
gabrielageorgieva <gabriela.georgieva@justice.gov.uk>
2026-03-20 14:15:11 +0000

    check for firm role and name

--- tests/features/step_defnitions/content.steps.ts
+++ tests/features/step_defnitions/content.steps.ts
@@ -34,14 +34,19 @@ Given('I am on the bulk submission landing page', async function (this: CustomWo
 
 Then('the page content matches {string}', async function (this: CustomWorld, fixtureName: string) {
   const fixturePath = path.resolve('tests/data/content_div', fixtureName);
-  const expectedHtml = await fs.readFile(fixturePath, 'utf8');
+  let expectedHtml = await fs.readFile(fixturePath, 'utf8');
 
   const mainContent = this.page!.locator('main#main-content');
   await mainContent.waitFor({state: 'visible', timeout: 30000});
-  const actualHtml = await mainContent.evaluate((node) => node.outerHTML);
+  let actualHtml = await mainContent.evaluate((node) => node.outerHTML);
+  const organisationTitle = this.page!.locator('.moj-organisation-nav__title');
+  await organisationTitle.waitFor({ state: 'visible', timeout: 30000 });
 
+  // Assert format: "ORG NAME - 1234"
+
+  await expect(organisationTitle).toHaveText(/.+\s*-\s*\d+/);
   expect(normalizeHtml(actualHtml)).toBe(normalizeHtml(expectedHtml));
-  await this.attach(`✅ Page content matches ${fixtureName}`, 'text/plain');
+  await this.attach(`✅ Search page content matches ${fixtureName}`, 'text/plain');
 });
 
 Then('the search page content matches {string}', async function (this: CustomWorld, fixtureName: string) {
@@ -74,6 +79,10 @@ Then('the search page content matches {string}', async function (this: CustomWor
     /<select[^>]*id="submission-period-select"[^>]*>[\s\S]*?<\/select>/,
     '<select id="submission-period-select">DYNAMIC_OPTIONS</select>'
  );
+
+  const organisationTitle = this.page!.locator('.moj-organisation-nav__title');
+  await organisationTitle.waitFor({ state: 'visible', timeout: 30000 });
+  await expect(organisationTitle).toHaveText(/.+\s*-\s*\d+/);
   await this.attach(`:white_check_mark: Search page content matches ${fixtureName}`, 'text/plain');
 });
 
